c7a6f58bb676678916dd5e7a21614556c9dabd56
Nazariy2 <nazrud23@gmail.com>
2024-10-24 19:01:42 +0300

    a

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
-const friends = ["Mango", "Poly", "kiwi", "Skibidi"];
+    // const friends = ["Mango", "Poly", "kiwi", "Skibidi"];
 // console.log(friends.join(","));
 
-let result = "";
+// let result = "";
 
-for (let i = 0; i < friends.length; i++) {
+// for (let i = 0; i < friends.length; i++) {
    
-    result += friends[i];
+//     result += friends[i];
     
   
-    if (i < friends.length - 1) {
-        result += ',';
-    }
-}
+//     if (i < friends.length - 1) {
+//         result += ',';
+//     }
+// }
 
 
-console.log(result);
+// console.log(result);
+
+const cards = [
+    "Card-1",
+    "Card-2",
+    "Card-3",
+    "Card-4",
+    "Card-5",
+]
+
+// const cardRemove = "Card-3";
+// const cardInsex = cards.indexOf(cardRemove);
+// console.log(cardInsex);
+// const removedCards = cards.slice(0,2);
+// const removeCards2 =cards.slice(3);
+// console.log(removedCards);
+// console.log(removeCards2);
+// const cardArray = removedCards.concat(removeCards2);
+// console.log(cardArray);
+
+
+// const  cardToInsert = cards.splice(5,0,"Card-6");
+
+// console.log(cards);
+
+const cardToUpdate = cards.splice(3,1, " Card-457");
+console.log(cards);
+
+
